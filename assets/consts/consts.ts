85a81d3bceb5720d550e2e37e981acccfebb27ca
Adam <AdamJankowski5@gmail.com>
2022-06-04 20:05:32 +0200

    first commit, added proper font for the app

---
+++ assets/consts/consts.ts
@@ -0,0 +1,21 @@
+import { Dimensions } from "react-native";
+const { width, height } = Dimensions.get("window");
+
+export const FONTS = {
+  h1: {
+    fontSize: 24,
+    fontFamily: "PressStart2P_400Regular",
+  },
+  h2: {
+    fontSize: 20,
+    fontFamily: "PressStart2P_400Regular",
+  },
+  h3: {
+    fontSize: 16,
+    fontFamily: "PressStart2P_400Regular",
+  },
+  h4: {
+    fontSize: 12,
+    fontFamily: "PressStart2P_400Regular",
+  },
+};
